4a198c30967239156b4f68e09885078733b3a5f3
Marcin Dytko <paczken@MacBook-Pro-Marcin.local>
2022-11-23 21:03:24 +0100

    Projcet Created

---
+++ TheBindingOfIssac/TheBindingOfIssac/TheBindingOfIssacApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheBindingOfIssacApp.swift
+//  TheBindingOfIssac
+//
+//  Created by Marcin Dytko on 23/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TheBindingOfIssacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
