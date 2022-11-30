378e141f733507256781301191b3e676480c22b9
Marcin Dytko <paczken@MacBook-Pro-Marcin.local>
2022-12-01 00:02:21 +0100

    UiChanges

--- TheBindingOfIssac/TheBindingOfIssac/ContentView.swift
+++ TheBindingOfIssac/TheBindingOfIssac/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    let menuCards = MenuCards()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("ISSAC!")
+        NavigationView{
+            
+            ScrollView{
+                
+                Rectangle()
+                    .frame(height: 2)
+                    .foregroundColor(.gray)
+                    .padding(.vertical)
+                
+                Image("FourSoulsLogo")
+                    .resizable()
+                    .scaledToFit()
+                ForEach(menuCards.cards, id: \.self){ card in
+                    HStack{
+                        Image("\(card)")
+                            .resizable()
+                            .frame(width: 70, height: 96)
+                            .padding(.horizontal, 16)
+                        
+                        
+                        NavigationLink{
+                            Text("something")
+                        } label: {
+                            Text(card)
+                                .font(.system(size: 36, weight: .semibold))
+                                .foregroundColor(.white)
+                            
+                            Spacer()
+                            
+                        }
+                        
+                    }
+                }
+            }
+        
+                // MENU enter button
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar{
+                    ToolbarItem{
+                        Button("Menu"){
+                            
+                        }
+                        .foregroundColor(.red)
+                        
+                    }
+                  
+                }
+                
+                .toolbarBackground(Color(red: 0.1, green: 0.1, blue: 0.2), for: .navigationBar)
+            
+       
+            .background(Color(red: 0.1, green: 0.1, blue: 0.2))
         }
-        .padding()
+        
     }
 }
 
---
+++ TheBindingOfIssac/TheBindingOfIssac/MenuCards.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCards.swift
+//  TheBindingOfIssac
+//
+//  Created by Marcin Dytko on 30/11/2022.
+//
+
+import Foundation
+
+struct MenuCards {
+    let cards = ["Character Card","Loot Card", "Monster Card", "Treasure Card","Bonus Soul"]
+    
+}
